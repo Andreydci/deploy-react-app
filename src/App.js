0787dbbdf40c7afef8f94c6303a6b14745061c99
Andreydci <andreyshhilkovjr@gmail.com>
2021-12-09 11:00:47 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Abut Endless
+          Another Change with versa
         </p>
         
       </header>
