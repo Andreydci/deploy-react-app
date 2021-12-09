e9a2e0e0e6407759941597200d1671da1a2478f2
Andreydci <andreyshhilkovjr@gmail.com>
2021-12-09 10:02:16 +0100

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Songs fom second floor
+          Abut Endless
         </p>
         
       </header>
